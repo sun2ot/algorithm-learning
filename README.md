42f2336995039221821a29a57d994f51b371bef4
sun2ot <84566015+sun2ot@users.noreply.github.com>
2023-12-09 18:59:09 +0800

    update

---
+++ README.md
@@ -0,0 +1,7 @@
+## Introduction
+
+C language implementation of the Algorithm Analysis and Design course at North Minzu University.
+
+## Process
+
+- [x] 分治法
--- divide-and-conquer/chessboard_covering.cpp
+++ divide-and-conquer/chessboard_covering.cpp
@@ -27,44 +27,53 @@ void printBoard() {
     }
 }
 
-// 棋盘覆盖函数
+
+/**
+ * 在棋盘上放置覆盖物
+ * 
+ * @param tr 当前棋盘的左上角行坐标(row)
+ * @param tc 当前棋盘的左上角列坐标(column)
+ * @param dr 特殊方格行坐标
+ * @param dc 特殊方格列坐标
+ * @param size 当前棋盘的大小
+ */
 void chessboard(int tr, int tc, int dr, int dc, int size) {
     if (size == 1) {
         return;
     }
 
-    int t = tile++;
-    int s = size / 2;
+    int t = tile++; // L型骨牌编号, 自增确保编号唯一
+    int s = size / 2; // 分割后棋盘大小
 
-    // 覆盖左上角子棋盘
-    if (dr < tr + s && dc < tc + s) {
-        chessboard(tr, tc, dr, dc, s);
-    } else {
-        board[tr + s - 1][tc + s - 1] = t;
-        chessboard(tr, tc, tr + s - 1, tc + s - 1, s);
+    // 处理左上角子棋盘
+    if (dr < tr + s && dc < tc + s) {  // 特殊方格在此棋盘中, 直接递归处理
+        chessboard(tr, tc, dr, dc, s); 
+    } else {  // 特殊方格不在此棋盘中, 先覆盖再处理
+        board[tr + s - 1][tc + s - 1] = t; // 用t号L型骨牌覆盖右下角方格
+        chessboard(tr, tc, tr + s - 1, tc + s - 1, s); // 处理其余方格
     }
 
-    // 覆盖右上角子棋盘
+    // 处理右上角子棋盘
     if (dr < tr + s && dc >= tc + s) {
         chessboard(tr, tc + s, dr, dc, s);
     } else {
-        board[tr + s - 1][tc + s] = t;
+        board[tr + s - 1][tc + s] = t; // 覆盖左下角
         chessboard(tr, tc + s, tr + s - 1, tc + s, s);
     }
 
-    // 覆盖左下角子棋盘
+    // 处理左下角子棋盘
     if (dr >= tr + s && dc < tc + s) {
         chessboard(tr + s, tc, dr, dc, s);
     } else {
-        board[tr + s][tc + s - 1] = t;
+        board[tr + s][tc + s - 1] = t; // 覆盖右上角
         chessboard(tr + s, tc, tr + s, tc + s - 1, s);
     }
 
-    // 覆盖右下角子棋盘
+    // 处理右下角子棋盘
     if (dr >= tr + s && dc >= tc + s) {
         chessboard(tr + s, tc + s, dr, dc, s);
     } else {
-        board[tr + s][tc + s] = t;
+        board[tr + s][tc + s] = t; // 覆盖左上角
         chessboard(tr + s, tc + s, tr + s, tc + s, s);
     }
 }
