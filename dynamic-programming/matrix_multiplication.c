121f51f95c60ad71021a7f21eab607ee946510f2
sun2ot <84566015+sun2ot@users.noreply.github.com>
2023-12-15 14:41:18 +0800

    finish

--- dynamic-programming/matrix_multiplication.c
+++ dynamic-programming/matrix_multiplication.c
@@ -20,15 +20,15 @@ int matrixChainOrder(int dimensions[], int n, int dp[MAX_MATRICES][MAX_MATRICES]
     }
 
     // 计算子问题的最优解和划分点
-    for (int len = 2; len <= n; len++) {
-        for (int i = 1; i <= n - len + 1; i++) {
-            int j = i + len - 1;
-            dp[i][j] = INT_MAX;
+    for (int len = 2; len <= n; len++) {  // 子问题规模
+        for (int i = 1; i <= n - len + 1; i++) {  // 子问题的起始位置
+            int j = i + len - 1;  // 子问题的结束位置
+            dp[i][j] = INT_MAX;  // 初始化最优值为最大值
 
-            for (int k = i; k < j; k++) {
+            for (int k = i; k < j; k++) {  // 子问题的划分点
                 int cost = dp[i][k] + dp[k + 1][j] + dimensions[i - 1] * dimensions[k] * dimensions[j];
-                if (cost < dp[i][j]) {
-                    dp[i][j] = cost;
+                if (cost < dp[i][j]) {  
+                    dp[i][j] = cost;  // 更新最优值
                     split[i][j] = k; // 记录最优划分点
                 }
             }
@@ -39,15 +39,22 @@ int matrixChainOrder(int dimensions[], int n, int dp[MAX_MATRICES][MAX_MATRICES]
     return dp[1][n];
 }
 
-// 输出最优解
+
+/**
+ * 输出最优解
+ * 
+ * @param split 分割矩阵的位置
+ * @param i 矩阵的起始位置
+ * @param j 矩阵的结束位置
+ */
 void printOptimalParenthesis(int split[MAX_MATRICES][MAX_MATRICES], int i, int j) {
-    if (i == j) {
+    if (i == j) {  // 矩阵只有一个时，直接输出
         printf("A%d", i);
-    } else {
-        printf("(");
-        printOptimalParenthesis(split, i, split[i][j]);
-        printOptimalParenthesis(split, split[i][j] + 1, j);
-        printf(")");
+    } else {  // 矩阵有多个时，递归输出
+        printf("(");  // 输出左括号
+        printOptimalParenthesis(split, i, split[i][j]);  // 输出左边的矩阵链的
+        printOptimalParenthesis(split, split[i][j] + 1, j);  // 输出右边的矩阵链
+        printf(")");  // 输出右括号
     }
 }
 
